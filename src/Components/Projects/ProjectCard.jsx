2a5946ff603abacd468e4164a8046bac8d3a15c6
YeasinRiyad <mazumderyeasin98@gmail.com>
2024-11-18 18:17:02 +0600

    New Project Add Successfully

--- src/Components/Projects/ProjectCard.jsx
+++ src/Components/Projects/ProjectCard.jsx
@@ -1,12 +1,11 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
-const ProjectCard = ({ title, main }) => {
+const ProjectCard = ({ title, main,img,demoLink,sourceLink }) => {
   return (
-    <div className="p-3 md:p-6 flex flex-col w-80 bg-[#0c0e19] shadow-xl shadow-slate-900 rounded-2xl">
-      {/* Corrected Image URL */}
+    <div className="p-3 md:p-6 flex flex-col justify-between w-80 bg-[#0c0e19] shadow-xl shadow-slate-900 rounded-2xl">
       <img
         className="p-4"
-        src="https://i.ibb.co/8sDvcc5/20190731-Pic-Blogbeitrag-Onboarding.webp"
+        src={img}
         alt="Project"
       />
       <h3 className="px-4 text-xl md:text-2xl font-bold leading-normal">
@@ -14,17 +13,15 @@ const ProjectCard = ({ title, main }) => {
       </h3>
       <p className="px-4 text-sm md:text-md leading-tight py-2">{main}</p>
       <div className="mt-2 p-2 md:p-4 flex gap-2 md:gap-4">
-        {/* Correct linking for the "Demo" using anchor tag */}
         <a
-          href="https://my-assets-management-app.web.app/"
+          href={demoLink}
           className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]"
           target="_blank" // Open in a new tab
-          rel="noopener noreferrer"
         >
           Demo
         </a>
         {/* Placeholder button for "Source Code" */}
-        <a href="https://github.com/yeasin-riyad/Asset-Management-System-Client" className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]">
+        <a href={sourceLink} className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]">
           Source Code
         </a>
       </div>
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -3,17 +3,29 @@ import ProjectCard from "./ProjectCard";
 const Projects = () => {
   return (
     <div id="Projects" className=" text-white  ">
-      <h1 className="text-2xl flex justify-center  md:text-4xl text-white font-bold">Projects</h1>
+      <h1 className="text-2xl flex justify-center  md:text-4xl text-white font-bold">
+        Projects
+      </h1>
       <div className="py-12 px-8 flex justify-center gap-5">
         <ProjectCard
           title="Assets Management Website"
           main="this is a Asset Management website created in React.js, Express.js, MongoDb Database, FireBase Authentication and used some component library."
-        />
-        {/* <ProjectCard
-          title="Youtue Clone"
-          main="this is a bloggin website created in next js and used some component library used some component library"
+          img="https://i.ibb.co/8sDvcc5/20190731-Pic-Blogbeitrag-Onboarding.webp"
+          demoLink={"https://my-assets-management-app.web.app/"}
+          sourceLink={
+            "https://github.com/yeasin-riyad/Asset-Management-System-Client"
+          }
         />
         <ProjectCard
+          title="Restaurant Management App"
+          main="this is a Restaurant Management app created in React.js and Tailwind Css.Welcome to the Restaurant Management project! This web application allows users to manage restaurant-related operations and enhance customer experience with features like table booking, contact forms, and more."
+          img={
+            "https://i.ibb.co.com/ydQ181k/pexels-bertellifotografia-16120160-1.jpg"
+          }
+          demoLink={"https://restaurant-management-8rji.vercel.app/"}
+          sourceLink={"https://github.com/yeasin-riyad/Restaurant-Management"}
+        />
+        {/* <ProjectCard
           title="Netflix Clone"
           main="this is a blogging website created this ijsdjf l in next js and used some component library used some component library"
         /> */}
